af800505ab8eccb223420cd1eb8ff694c0821a0a
numerator6799 <>
2024-09-01 19:46:58 -0300

    feat: read creds from environment

--- helpers.py
+++ helpers.py
@@ -1,9 +1,19 @@
 import sys
+import os
 from console_helper import print_error
 
 #TODO: try to get from environment variables
 def get_credentials(args):
-    if args.username is None:
-        print_error("Username and password required")
+    username = os.getenv("BSKY_USERNAME")
+    if username is None:
+        username=args.username
+    if username is None:
+        print_error("Username required")
         sys.exit(1)
-    return (args.username, args.password)
+    password = os.getenv("BSKY_PASSWORD")
+    if password is None:
+        password=args.password
+    if password is None:
+        print_error("Password required")
+        sys.exit(1)
+    return (username, password)
