83f39cf1616c70e882c6cd0ed5ed7cf55cb41d72
numerator6799 <>
2024-09-01 09:35:45 -0300

    feat: follow who liked a post or comment

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 import sys
-from atproto import Client
+import time
+from atproto import Client # type: ignore
 
+WAIT_TIME_BETWEEN_PAGINATED_CALLS=3
+WAIT_TIME_BETWEEN_FOLLOWS=3
 #TODO: split into another file
+
+def get_paginated(func, cid, list_prop):
+    stop = False
+    list = []
+    cursor=None
+    while not stop:
+        print(".", end=" ")
+        resp = func(cid, cursor=cursor, limit=100)
+        time.sleep(WAIT_TIME_BETWEEN_PAGINATED_CALLS)
+        list = list + resp[list_prop]
+        cursor = resp.cursor
+        if cursor == None:
+            stop = True
+    return list
+
+def follow_all(list, client, handle_prop="handle", did_prop="did"):
+    # maybe getting followers for comparison is not needed, but let's try to avoid
+    # too many calls to follow
+    follows = get_paginated(func=client.get_follows, cid=client.me.handle, list_prop="follows")
+    print("Got " + str(len(follows)) + " follows")
+    for index, p in enumerate(list):
+        already_follow=False
+        for index2, follow in enumerate(follows):
+            if follow.handle == p[handle_prop]:
+                already_follow=True
+                break
+        if already_follow:
+            print(bcolors.OKGREEN + "You already follow " + p[handle_prop] + bcolors.ENDC)
+        else:
+            print("Following " + p[handle_prop])
+            client.follow(p[did_prop])
+            time.sleep(WAIT_TIME_BETWEEN_FOLLOWS)
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -23,28 +59,26 @@ print(bcolors.OKGREEN + "Welcome, " + client.me.handle + bcolors.ENDC)
 #TODO: menu array, validate user input
 print("What would you like to do?")
 print("1 - Follow my followers")
+print("2 - Follow who liked a post or comment")
+# print("3 - Like all comments in post")
 choice = int(input("Enter your choice:"))
 
 #TODO: commands, command mapping
 if choice == 1:
-    #TODO: pagination
-    resp = client.get_followers(client.me.handle, cursor=None, limit=100)
-    followers = resp.followers
+
+    followers = get_paginated(func=client.get_followers, cid=client.me.handle, list_prop="followers")
     print("Got " + str(len(followers)) + " followers")
-    resp = client.get_follows(client.me.handle, cursor=None, limit=100)
-    follows = resp.follows
+    follows = get_paginated(func=client.get_follows, cid=client.me.handle, list_prop="follows")
     print("Got " + str(len(follows)) + " follows")
-    for index, follower in enumerate(followers):
-        already_follow=False
-        for index2, follow in enumerate(follows):
-            if follow.handle == follower.handle:
-                already_follow=True
-                break
-        if already_follow:
-            print(bcolors.OKGREEN + "You already follow " + follower.handle + bcolors.ENDC)
-        else:
-            print("Following " + follower.handle)
-            client.follow(follower.did)
-
-
-
+    follow_all(followers, client)
+if choice == 2:
+    element_id=input("Element id:")
+    author_handle=input("Author handle:")
+    post = client.get_post(element_id, author_handle)
+    print("Getting all likes for post " + post.value.text + ". Please wait...")
+    likes = get_paginated(func=client.get_likes, cid=post.uri, list_prop="likes")
+    print("Got " + str(len(likes)) + " likes")
+    users = [like.actor for like in likes]
+    follow_all(users, client)
+    
+    
