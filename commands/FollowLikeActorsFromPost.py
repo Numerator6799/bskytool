8c475dcfa90ff9b887dec3b0ac84510841c5f3fe
numerator6799 <>
2024-09-01 11:31:12 -0300

    feat: use own handle if blank to get post

--- commands/FollowLikeActorsFromPost.py
+++ commands/FollowLikeActorsFromPost.py
@@ -1,13 +1,15 @@
 from bluesky_helper import get_paginated, follow_all
 from commands.BaseCommand import BaseCommand
 
-class FollowLikeActorsFromElement(BaseCommand):
+class FollowLikeActorsFromPost(BaseCommand):
     def __init__(self, client):
-        BaseCommand.__init__(self, client, "Follow who liked post or comment")
+        BaseCommand.__init__(self, client, "Follow who liked post")
     
     def run(self):
-        element_id=input("Element id:")
-        author_handle=input("Author handle:")
+        element_id=input("Post id: ")
+        author_handle=input("Author handle (leave blank to use " + self.client.me.handle + "): ")
+        if len(author_handle) == 0:
+            author_handle=self.client.me.handle
         post = self.client.get_post(element_id, author_handle)
         print("Getting all likes for post " + post.value.text + ". Please wait...")
         likes = get_paginated(func=self.client.get_likes, cid=post.uri, list_prop="likes")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from console_helper import bcolors, printc
 from bluesky_helper import create_authenticated_client
 from commands.FollowAllFollowersCommand import FollowAllFollowersCommand
-from commands.FollowLikeActorsFromElement import FollowLikeActorsFromElement
+from commands.FollowLikeActorsFromPost import FollowLikeActorsFromPost
 
 #TODO: locale
 if(len(sys.argv) < 3):
@@ -12,7 +12,7 @@ if(len(sys.argv) < 3):
 client=create_authenticated_client(sys.argv[1], sys.argv[2])
 commands = {
     1: FollowAllFollowersCommand(client),
-    2: FollowLikeActorsFromElement(client),
+    2: FollowLikeActorsFromPost(client),
 }
 
 print("What would you like to do?")
