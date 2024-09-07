021193521d76440eec0abb0deeb2712bfe301cab
numerator6799 <>
2024-09-07 12:46:04 -0300

    feat: add usage script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python3 bsky.py bcache
+python3 bsky.py ffollowers
+python3 bsky.py fallmypostlikes
+
+# Usage: 
+# craete a urlwatch.txt
+# add links (one per line) to posts you want
+# to track and follow who liked them
+URL_FILE="urlwatch.txt"
+COMMAND="python3 bsky.py fpostlikes -e"
+
+while IFS= read -r url; do
+    echo "Running for $url"
+    $COMMAND $url
+done < "$URL_FILE"
+
